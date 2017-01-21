c184e798fc1563161128a6804fb3324958c65b68
Lexikos <unravelin@gmail.com>
2017-01-21 12:21:27 +1000

    Fix auto-indent for lpeg_ahk

--- lpeg_s4a.lua
+++ lpeg_s4a.lua
@@ -1,5 +1,5 @@
--- Override lexer.
 RegisterEvents({
+    -- Override lexer.
     OnClear = function()
         local lexer = 'lpeg_ahk' .. (props["ahk.platform"]:find("v2") and '2' or '1')
         if props["lexer.$(file.patterns.ahk)"] ~= lexer then
@@ -8,5 +8,14 @@ RegisterEvents({
                 editor:StartStyling(0, 0)
             end
         end
+    end,
+    -- Fix auto-indent for lpeg_ahk.
+    OnChar = function(ch)
+        if editor.LexerLanguage ~= "lpeg" then return end
+        if    ch == "\n" then  return AutoIndent_OnNewLine()
+        elseif ch == "{" then         AutoIndent_OnOpeningBrace()
+        elseif ch == "}" then         AutoIndent_OnClosingBrace()
+        end
+        return false
     end
 })
